a82b398cd04ed5cf8cbba4df4beff085ec82b5a5
Antoni Kępiński <antoni@kepinski.ch>
2023-04-20 15:57:54 +0200

    Sync Apr 20, 2023 15:57:36 CEST

--- dot.sh
+++ dot.sh
@@ -34,7 +34,7 @@ if [[ "$1" == 'sync' ]]; then
 	cd $DIRECTORY
 
 	git fetch --all
-	git reset --hard
+	git pull
 
 	for file in ${TO_SYNC[@]}; do
 		# Alternative: `gcp --parents` (requires `brew install coreutils`).
